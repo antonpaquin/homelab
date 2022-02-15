2b6b7c3c7dd51716f644d151a9c8fb05f13a7031
Anton Paquin <git@antonpaqu.in>
2022-02-14 23:34:42 -0800

    More authfail imgs

--- docker/auth-proxy/authfail_img.sh
+++ docker/auth-proxy/authfail_img.sh
@@ -3,4 +3,9 @@
 mkdir -p authfail_img
 wget -O authfail_img/0.png https://cdn.donmai.us/original/c1/ba/__inubashiri_momiji_touhou_drawn_by_bwsnowy__c1bade179dc4d9eb72eea0c8b0ef5b51.png
 wget -O authfail_img/1.png https://cdn.donmai.us/original/58/0f/__akechi_kokoro_tantei_opera_milky_holmes_drawn_by_toma_shinozaki__580f36fb2ed0e56efd6d1692b802f9bf.png
-wget -O authfail_img/2.jpg https://danbooru.donmai.us/data/original/3d/68/__oozora_subaru_hololive_drawn_by_homura_minori__3d680f18e4415a536b1d9f2947e07b4f.jpg
+wget -O authfail_img/2.jpg https://cdn.donmai.us/original/3d/68/3d680f18e4415a536b1d9f2947e07b4f.jpg
+wget -O authfail_img/3.png https://cdn.donmai.us/original/1c/35/__original_drawn_by_sakura_neko__1c35c8ddb985db21d9655afb3ac5e650.png
+wget -O authfail_img/4.jpg https://cdn.donmai.us/original/60/02/__e_roma_sword_girls__6002b20eb9c53d7c08eb9f5c1dae0eb3.jpg
+wget -O authfail_img/5.jpg https://cdn.donmai.us/original/5f/be/__oozora_subaru_hololive_drawn_by_shyi__5fbef5e48866e3abf32b70e5d31bf793.jpg
+wget -O authfail_img/6.jpg https://cdn.donmai.us/original/8b/c4/__kasumi_and_kasumi_kai_ni_kantai_collection_drawn_by_masukuza_j__8bc46b792264b2fe0b82a43c6b7e98ec.jpg
+wget -O authfail_img/7.jpg https://cdn.donmai.us/original/82/d7/__caeda_fire_emblem_and_1_more_drawn_by_ten_tenchan_man__82d740d2c20cc2fdd8bb6c1f9f9e853f.jpg
