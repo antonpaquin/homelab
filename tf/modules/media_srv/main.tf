a7f6c70c979e724e5dbd31fa7845f1dcfe52923d
Anton Paquin <git@antonpaqu.in>
2022-02-14 23:51:10 -0800

    so I've got this thing to no-auth serve files, but maybe that's not such a good idea now that I'm on the web? Eh, if you're reading this please ignore thanks

---
+++ tf/modules/media_srv/main.tf
@@ -0,0 +1,118 @@
+variable "domain" {
+  type = string
+}
+
+variable "media-pvc" {
+  type = string
+}
+
+locals {
+  namespace = "default"
+  host = "media-srv.${var.domain}"
+}
+
+resource "kubernetes_config_map" "media-srv" {
+  metadata {
+    name = "media-srv"
+    namespace = local.namespace
+  }
+  data = {
+    "default.conf" = <<EOF
+server {
+    listen 80;
+    location / {
+        autoindex on;
+        root /media/library;
+    }
+}
+EOF
+  }
+}
+
+resource "kubernetes_deployment" "media-srv" {
+  metadata {
+    name = "media-srv"
+    namespace = local.namespace
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = "media-srv"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "media-srv"
+        }
+      }
+      spec {
+        container {
+          name = "main"
+          image = "nginx"
+          volume_mount {
+            name = "media"
+            mount_path = "/media"
+          }
+          volume_mount {
+            name = "config"
+            mount_path = "/etc/nginx/conf.d"
+          }
+        }
+        volume {
+          name = "media"
+          persistent_volume_claim {
+            claim_name = var.media-pvc
+          }
+        }
+        volume {
+          name = "config"
+          config_map {
+            name = kubernetes_config_map.media-srv.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "media-srv" {
+  metadata {
+    name = "media-srv"
+    namespace = local.namespace
+  }
+  spec {
+    selector = {
+      app = "media-srv"
+    }
+    port {
+      port = 80
+      name = "http"
+    }
+  }
+}
+
+resource "kubernetes_ingress" "media-srv" {
+  metadata {
+    name = "media-srv"
+    namespace = local.namespace
+  }
+  spec {
+    rule {
+      host = local.host
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = kubernetes_service.media-srv.metadata[0].name
+            service_port = "http"
+          }
+        }
+      }
+    }
+  }
+}
+
+output "host" {
+  value = local.host
+}
