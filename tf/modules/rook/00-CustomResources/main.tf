41f391f0e00041b6b0a7207e7b7a0738538cd543
Anton Paquin <git@antonpaqu.in>
2022-02-14 23:44:19 -0800

    I dunno why I did this

--- tf/modules/rook/00-CustomResources/main.tf
+++ tf/modules/rook/00-CustomResources/main.tf
@@ -13,6 +13,7 @@ resource "helm_release" "rook" {
 
   chart = "rook-ceph"
   name = "rook-ceph"
+  version = "1.6.7"  # pinned in a fit of rage, mistaking the source of the error. Can probably be unpinned on next reset
   namespace = kubernetes_namespace.rook-ceph.metadata[0].name
   repository = "https://charts.rook.io/release"
 }
