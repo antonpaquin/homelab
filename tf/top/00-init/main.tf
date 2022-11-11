6f7f555c3511060b7a77ffd8f0036ab6ec9ac705
Anton Paquin <git@antonpaqu.in>
2022-11-10 18:09:40 -0800

    rework 00 for hakurei

--- tf/top/00-init/main.tf
+++ tf/top/00-init/main.tf
@@ -7,12 +7,16 @@
 #   source = "../../modules/calico"
 # }
 
+locals {
+  reimu_ip = "192.168.0.105"
+}
+
 module "coredns" {
   source = "../../modules/coredns"
   # Allow reimu to be used as DNS (requires bind9 module later, but that's OK)
   coredns-snippet = <<EOF
 k8s.local {
-    forward . 10.0.4.64
+    forward . ${local.reimu_ip}
 }
 EOF
 }
