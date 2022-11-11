450af608e90051fb7edb6cf7abf8a0d54c8fee49
Anton Paquin <git@antonpaqu.in>
2022-11-10 19:04:54 -0800

    tune values for cluster

--- tf/top/01-provisioners/main.tf
+++ tf/top/01-provisioners/main.tf
@@ -1,9 +1,9 @@
 module "nfs" {
   source = "../../../modules/app_infra/nfs"
   nfs_root = {
-    capacity = "3726Gi"
+    capacity = "7168Gi"
     hostPath = "/storage/root"
     node = "reimu-00"
-    node_ip = "192.168.0.104"
+    node_ip = "192.168.0.105"
   }
 }
