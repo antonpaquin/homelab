2b5e3652eb610768d40368ae84873a1ae468f700
Anton Paquin <git@antonpaqu.in>
2022-11-10 21:16:09 -0800

    more move

--- tf/top/03-application/heimdall.tf
+++ tf/top/03-application/heimdall.tf
@@ -1,5 +1,5 @@
 module "heimdall" {
-  source = "../../modules/heimdall"
+  source = "../../modules/app/heimdall"
   domain = local.domain
   heimdall_apps = [
     # kubectl exec svc/heimdall -c main -- ls /config/www/icons
