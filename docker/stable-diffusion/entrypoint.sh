198c2873871afc241e3dac4f4ba7b680b016b647
Anton Paquin <git@antonpaqu.in>
2022-11-12 22:48:18 -0800

    add some more models

--- docker/stable-diffusion/entrypoint.sh
+++ docker/stable-diffusion/entrypoint.sh
@@ -12,4 +12,15 @@ for f in /models/transformers/*; do
     ln -s $f /root/.cache/huggingface/transformers/$(basename $f)
 done
 
+mkdir -p /root/.cache/torch/hub/checkpoints/
+for f in /models/torch_checkpoints/*; do
+    ln -s $f /root/.cache/torch/hub/checkpoints/$(basename $f)
+done
+
+mkdir -p /root/.cache/clip
+for f in /models/clip/*; do
+    ln -s $f /root/.cache/clip/$(basename $f)
+done
+model_base_caption_capfilt_large.pth .
+
 python launch.py "$@"
