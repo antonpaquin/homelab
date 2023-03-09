5dcad6c700a7c7dd8363b36b2165b9a017700bcd
Anton Paquin <git@antonpaqu.in>
2023-03-09 11:05:29 -0800

    Add providers

---
+++ tf/top/azumanga/00-init/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      version = "2.15.0"
+    }
+    kubernetes-alpha = {
+      version = "0.4.1"
+    }
+    random = {
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "/home/user/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "/home/user/.kube/config"
+  }
+}
+
+
