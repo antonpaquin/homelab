ab48ad2e7d3f4a927a7303478589ffb27b8ef504
Anton Paquin <git@antonpaqu.in>
2023-03-09 16:53:09 -0800

    add modules path to venv, does that work?

---
+++ pulumi/top/azumanga/pulumi_path.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+MODULES_PATH="$(cd ../../modules && pwd)"
+
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+	echo "Activate the virtual env first" 1>&2
+	exit 1
+fi
+
+SITE_PACKAGES="$(python -c 'import site; print([c for c in site.getsitepackages() if c.endswith("site-packages")][0])')"
+
+echo "$MODULES_PATH" > "$SITE_PACKAGES/pulumi_modules.pth"
